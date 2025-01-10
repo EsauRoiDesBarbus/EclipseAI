fab9692c92e75c62cad21d8392d2610c71edf5a2
Etienne Bertin <etienne.bertin@ensta-paristech.fr>
2025-01-10 16:16:22 +0100

    outline of automatic data generation

---
+++ data_generation.py
@@ -0,0 +1,111 @@
+from eclipseCpp import battleBuilder, Weapons
+
+
+
+class ShipData:
+    def __init__ (self, number, type, initiative, hull, computer, shield, canons, missiles):
+        self._number = number
+        self._type = type
+        self._initiative = initiative
+        self._hull = hull
+        self._computer = computer
+        self._shield = shield
+        self._canons = canons
+        self._missiles = missiles
+        self._regen = 0
+
+    def toVector (self):
+        # writes the ship as a vector for the neural network
+        # type is not written here as it is implicit as the position in the battle vector
+        vec = [self._number, self._initiative, self._hull, self._computer, self._shield, self._regen]
+        vec+= [self._canons[0]  ,self._canons[1]  ,self._canons[2]  ,self._canons[3]  ,self._canons[4]]
+        vec+= [self._missiles[0],self._missiles[1],self._missiles[2],self._missiles[3],self._missiles[4]]
+        return vec
+
+def emptyShip ():
+    return ShipData(0, "INT", 0, 0, 0, 0, [0,0,0,0,0], [0,0,0,0,0])
+
+class BonusData:
+    def __init__ (self, is_npc, antimatter_splitter):
+        # saves battle modifiers as 0 (False) and 1 (True)
+        if (is_npc == True)or(is_npc == 1):
+            self._is_npc = 1
+        else:
+            self._is_npc = 0
+        if (antimatter_splitter == True)or(antimatter_splitter == 1):
+            self._antimatter_splitter = 1
+        else:
+            self._antimatter_splitter = 0
+
+    def toVector (self):
+        # writes the bonus as a vector for the neural network
+        return [self._is_npc, self._antimatter_splitter]
+
+
+class BattleData:
+    def __init__ (self, attacker_ships, attacker_bonus, defender_ships, defender_bonus):
+        # mirrors the constructor of ShipBattleStates in eclipseCpp
+        self._attacker_ships = attacker_ships
+        self._defender_ships = defender_ships
+
+        self._attacker_bonus = attacker_bonus
+        self._defender_bonus = defender_bonus
+
+    def toVector (self):
+        # writes the battle as a vector for the neural network
+        vec = []
+        for type in ["INT", "CRU", "DRE"]:
+            no_ship=True
+            for ship in self._attacker_ships:
+                if (ship._type == type)and(no_ship):
+                    vec+= ship.toVector()
+                    no_ship=False
+            if (no_ship):
+                vec+=emptyShip().toVector()
+            
+        vec+= self._attacker_bonus.toVector()
+
+
+        for type in ["INT", "CRU", "DRE", "SBA"]:
+            no_ship=True
+            for ship in self._defender_ships:
+                if (ship._type == type)and(no_ship):
+                    vec+= ship.toVector()
+                    no_ship=False
+            if (no_ship):
+                vec+=emptyShip().toVector()
+
+        vec+= self._defender_bonus.toVector()
+
+        return vec
+    
+    def signature (self):
+        # use injective hash function on toVector to detect and remove duplicates
+        return hash(tuple(self.toVector()))
+    def solveBattle (self):
+        # use the eclipseCpp library to solve the battle
+        return 0
+    def toCSV (self):
+        # the format is signature, toVector, Battleresult
+        return 0
+
+def randomShip (type):
+    #todo make it random
+    if (type=="INT"):
+        return ShipData(1, "INT", 3, 0, 0, 0, [1,0,0,0,0], [0,0,0,0,0])
+    if (type=="CRU"):
+        return ShipData(1, "CRU", 2, 1, 1, 0, [1,0,0,0,0], [0,0,0,0,0])
+    if (type=="DRE"):
+        return ShipData(1, "DRE", 1, 2, 1, 0, [2,0,0,0,0], [0,0,0,0,0])
+    if (type=="SBA"):
+        return ShipData(1, "SBA", 4, 2, 1, 0, [1,0,0,0,0], [0,0,0,0,0])
+
+def randomBattle ():
+    return BattleData([randomShip("INT"), randomShip("CRU")], BonusData (False, False), [randomShip("DRE")], BonusData (False, False))
+
+
+battle = randomBattle()
+
+print (battle.toVector())
+print (len(battle.toVector()))
+print (battle.signature())
